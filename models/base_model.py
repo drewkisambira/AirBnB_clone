daacd1fa995c3e300928f88b5cfb66675f4602fe
Davis Tumuhaise <tumuhaise@gmail.com>
2022-11-23 00:53:46 +0300

    upload base_model.py and init

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" module creates BaseModel class that defines all
+common attributes for other classes
+"""
+
+
+import uuid
+import datetime
+
+
+class BaseModel:
+    def __init__(self, created_at, updated_at, id=uuid.uuid4()):
+        self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def __str__(self):
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+
+    def save(self):
+        """define method that updates updated_at with current datetime
+        """
+
+        updated_at = datetime.now()
+        return updated_at
+
+    def to_dict(self):
+        return {}
