8abdba02cbfdd78e4ae872def94e1f5c75126382
Davis Tumuhaise <tumuhaise@gmail.com>
2022-11-24 13:50:23 +0300

    update basemodel.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,21 @@ class BaseModel:
     define base class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    value == self.id
+                if key == 'created_at':
+                    value == self.created_at
+                if key == 'updated_at':
+                    value == self.updated_at
+                
     def __str__(self):
         return "[{:s}] ({:s}) {:s}".format(
             self.__class__.__name__, self.id, str(self.__dict__))
