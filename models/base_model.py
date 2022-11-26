f97db627fdb068706857820d28b54495d8f7badf
Davis Tumuhaise <tumuhaise@gmail.com>
2022-11-27 01:19:00 +0300

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,28 @@ class BaseModel:
     define base class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """
+        defining constructor
+        """
 
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    val = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, k, val)
+                elif k != '__class__':
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        method torepresent calss objects as a string
+        """
+
         return "[{:s}] ({:s}) {:s}".format(
             self.__class__.__name__, self.id, str(self.__dict__))
 
@@ -29,6 +44,9 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        method returns a dictionary containing key/values of __dict__
+        """
 
         new_dict = self.__dict__.copy()
         new_dict.update(__class__=self.__class__.__name__)
