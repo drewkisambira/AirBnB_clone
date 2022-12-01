11188e148a193225648e08528bd9e9242934d1e8
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-01 11:12:33 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,8 +4,15 @@ define class that serialisses instances to JSON and
 deserilizes JSON fitle to instances
 """
 
-
+import os
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage():
